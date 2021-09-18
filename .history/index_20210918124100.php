00e9eafbb5449f8cc1459df12a8bac4a6b4c6551
Motiejus <motiejus.baliukonis@vka.lt>
2021-09-18 12:42:34 +0300

    *pakeistas atvaizdavimo kodas

--- .history/index_20210918124100.php
+++ .history/index_20210918124100.php
@@ -37,28 +37,23 @@
 
         <button id="show"> Show Clients AJAX</button>
         <div id="output">
-        <?php
+        $sql = "SELECT * FROM klientai
+    WHERE 1
+    ORDER BY klientai.ID DESC
+    LIMIT 50
+    ";
 
-
-$sql = "SELECT * FROM klientai
-WHERE 1
-ORDER BY klientai.ID DESC
-LIMIT 50
-";
-
-$result = $conn->query($sql);// uzklausos vykdymas
-echo "<table class='table table-striped'>";
-while($clients = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    echo "<td>". $clients["ID"]. "</td>";
-    echo "<td>". $clients["vardas"]. "</td>";
-    echo "<td>". $clients["pavarde"]. "</td>";
-    echo "<td>". $clients["teises_id"]. "</td>";
-    echo "</tr>";
-}
-echo "</table>";
-
-?>
+    $result = $conn->query($sql);// uzklausos vykdymas
+    echo "<table class='table table-striped'>";
+    while($clients = mysqli_fetch_array($result)) {
+        echo "<tr>";
+        echo "<td>". $clients["ID"]. "</td>";
+        echo "<td>". $clients["vardas"]. "</td>";
+        echo "<td>". $clients["pavarde"]. "</td>";
+        echo "<td>". $clients["teises_id"]. "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
         </div>
 
         
